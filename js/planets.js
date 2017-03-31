f7d4a68b6b6d3b597e6f85d5d914fd624eee2a3d
Lucas Henrique <lucashenrique.he@gmail.com>
2017-03-30 23:37:38 -0300

    Adjusting variables, orders and more

--- js/planets.js
+++ js/planets.js
@@ -1,3 +1,10 @@
+/*
+*	Planet Earth in THREE.js
+*
+* Author: Lucas Henrique
+* Author URI: https://github.com/lurique
+*/
+
 var Three = Three || {};
 
 (function() {
@@ -6,16 +13,30 @@ var Three = Three || {};
 
 	Three.Planets = {
 
+		// Common
+		width: window.innerWidth,
+		height: window.innerHeight,
+
+		// ThreeJS variables
+		scene: '',
+		camera: '',
+		renderer: '',
+		light: '',
+
+		// Planets variables
+		radius: 0,
+		segments: 0,
+		rotation: 0,
+		planet: '',
+		clouds: '',
+		particles: '',
+
+		// Texture loader
+		loader: new THREE.TextureLoader(),
+
 		init: function() {
-			this.setVariables();
 			this.setScene();
 			this.setRender();
-			this.setPlanets();
-		},
-
-		setVariables: function() {
-			this.width = window.innerWidth;
-			this.height = window.innerHeight;
 		},
 
 		setScene: function() {
@@ -24,17 +45,33 @@ var Three = Three || {};
 			this.scene = new THREE.Scene();
 			this.camera = new THREE.PerspectiveCamera(75, self.width / self.height, 0.1, 1000);
 			this.renderer = new THREE.WebGLRenderer();
+			this.light = new THREE.DirectionalLight(0xFFFFFF, 1);
 		},
 
 		setRender: function() {
 			var self = Three.Planets;
 
+			// Setting camera default position
+			self.camera.position.z = 1.5;
+
+			// Creating canvas
 			self.renderer.setSize(self.width, self.height);
 			document.body.appendChild(self.renderer.domElement);
+
+			// Rendering ambient lights
+			self.light.position.set(5,3,5);
+			self.scene.add(self.light);
+
+			// Rendering planets
+			self.setPlanets.planetEarth();
 		},
 
-		setPlanets: function() {
-			alert('Oi');	
+		setPlanets: {
+			planetEarth: function() {
+				var self = Three.Planets;
+
+				alert('Function is working');
+			}
 		}
 
 	}
